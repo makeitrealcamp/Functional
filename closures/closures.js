b5e49c98f13f64d7d82c71ff920bd25d235ccc16
diegozwolf <diegozuluaga88@gmail.com>
2016-09-19 13:41:18 -0500

    resuelto hasta paso 5 maria-diego

--- closures/closures.js
+++ closures/closures.js
@@ -15,6 +15,29 @@ function nonSense(string){
 	var blab = function(){
 		alert(string);
 	}
-	
+	setTimeout(blab, 2000);
 }
 
+
+// Paso 3
+
+function nonSense(string){
+	var blab = function(){
+		alert(string);
+	}
+	return blab;
+
+}
+var blabLater = nonSense('hola');
+
+var blabAgainLater = nonSense('hey')
+
+// Paso 4
+
+  var nameGenerator = function(firstName){     
+
+      var innerFunction = function(lastName) {
+      		console.log(firstName + ' ' + lastName)
+      }; return innerFunction;    
+  }
+// Paso 5
